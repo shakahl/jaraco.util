482e867683239085adab9f509983fa05797c2881
jaraco <none@none>
2005-01-25 15:43:19 +0000

    Some miscellaneous functionality taken from the former EnvMon/Populators.py.  ********************** Label: "1.2" User: Jaraco       Date: 23-08-02   Time: 17:13 Labeled Label comment: Implements a robust processing service, enhanced SQL object handling, Web, FTP, and File-based data processing.

---
+++ fstools.py
@@ -0,0 +1,18 @@
+# fstools.py:
+#  tools for working with files and file systems
+
+# return a filename possibly with a number appended to it so that it is
+#  unique in the directory
+def GetUniqueFilename( filename, directory ):
+	basename, ext = os.path.splitext( filename )
+	# save the file with the original name.  If it already exists,
+	#  append a number to the end so as not to overwrite the original
+	number = 0
+	newFilename = filename
+	while 1:
+		destination = os.path.join( directory, newFilename )
+		if not os.path.exists( destination ): break
+		number += 1
+		newFilename = '%s (%d)%s' % ( basename, number, ext )
+
+	return newFilename
