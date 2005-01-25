5df8f8e40e4fb78f42ae546d2d99d6b84d65ee08
jaraco <none@none>
2005-01-25 15:35:17 +0000

    Cleaned up the code. Removed all loops except while(1). Simplified the FileChangeNotifierException. Reworked thread processing a bit. Fixed some bugs, tightened timing.

--- FileChange.py
+++ FileChange.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-
-# modified 2001-10-19
-
-import os, sys
+import os, sys, time
 from threading import Thread
-import time
 import traceback
 from stat import *
 
@@ -14,13 +9,17 @@ from win32api import *
 from win32event import *
 from win32con import FILE_NOTIFY_CHANGE_LAST_WRITE
 
-class FileChangeNotifierException:
-    def __init__( self, value='unspecified error' ):
-        self.value = value
 
-    def __str__( self ):
-        return self.value
+class ChangedSinceFilter( object ):
+	def __init__( self, cutoff ):
+		self.cutoff = cutoff
 
+	def __call__( self, filepath ):
+		last_mod = os.stat( filepath )[ST_MTIME]
+		return last_mod >= self.cutoff
+
+class FileChangeNotifierException( Exception ):
+	pass
 
 # FileChangeNotifier provides a simple interface that calls the handler
 #  for each file that is updated matching root\filter
@@ -28,69 +27,85 @@ class FileChangeNotifierException:
 # Note: files that are updated while the handler is processing are not caught
 
 class FileChangeNotifier( Thread ):
-    def __init__( self, root = os.sep, filter = '*', handler = lambda: None ):
-        # init thread stuff
-        Thread.__init__( self )
-        
-        # assign the root, verify it exists, and make sure it ends
-        #  in a backslash (\)
-        self.root = root
-        if not os.path.isdir( self.root ):
-            raise FileChangeNotifierException( 'Root directory %s does not exist' % self.root )
-        if not self.root[-1] == os.sep:
-            self.root += os.sep
+	def __init__( self, root = '.', filter = '*', handler = lambda file: None ):
+		# init thread stuff
+		Thread.__init__( self )
+		# set it as a daemon thread so that it doesn't block waiting to close.
+		#  I tried setting __del__( self ) to .Quit(), but unfortunately, there are
+		#  references to this object in the win32api stuff, so __del__ never gets
+		#  called.
+		self.setDaemon( 1 )
+		
+		# assign the root, verify it exists
+		self.root = root
+		if not os.path.isdir( self.root ):
+			raise FileChangeNotifierException( 'Root directory %s does not exist' % self.root )
 
-        # assign the filter and handler
-        self.filter = filter
-        self.Handle = handler
+		# assign the filter and handler
+		self.filter = filter
+		self.Handle = handler
 
-        self.QuitEvent = CreateEvent( None, 0, 0, None )
+		self.QuitEvent = CreateEvent( None, 0, 0, None )
 
-    def Quit( self ):
-        SetEvent( self.QuitEvent )
+	def Quit( self ):
+		SetEvent( self.QuitEvent )
 
-    def run( self ):
-        # set up to monitor the directory tree specified
-        hChange = FindFirstChangeNotification( self.root, 0, \
-                                               FILE_NOTIFY_CHANGE_LAST_WRITE )
+	def run( self ):
+		# set up to monitor the directory tree specified
+		hChange = FindFirstChangeNotification( self.root, 0, \
+											   FILE_NOTIFY_CHANGE_LAST_WRITE )
 
-        # make sure it worked; if not, bail
-        if hChange == INVALID_HANDLE_VALUE:
-            print 'Error getting Change Notification Handle'
-            raise FileChangeNotifierException( 'Could not set up directory change notification' )
+		# make sure it worked; if not, bail
+		if hChange == INVALID_HANDLE_VALUE:
+			raise FileChangeNotifierException, 'Could not set up directory change notification'
 
-        # continuously monitor files and handle them if they've changed
-        try:
-            while (1):
-                # make account of when we started waiting for updates
-                lastCheckedTime = time.time()
-                # block (sleep) until something changes in the
-                #  target directory or a quit is requested.
-                # timeout so we can catch keyboard interrupts or other exceptions
-                result = WAIT_TIMEOUT
-                while result == WAIT_TIMEOUT:
-                    result = WaitForMultipleObjects( ( hChange, self.QuitEvent ), 0, 1000 )
+		# continuously monitor files and handle them if they've changed
+		try:
+			# make account of when we started waiting for updates
+			self.lastCheckedTime = time.time()
+			while (1):
+				# block (sleep) until something changes in the
+				#  target directory or a quit is requested.
+				# timeout so we can catch keyboard interrupts or other exceptions
+				result = WAIT_TIMEOUT
+				while result == WAIT_TIMEOUT:
+					result = WaitForMultipleObjects( ( hChange, self.QuitEvent ), 0, 1000 )
 
-                if result == WAIT_OBJECT_0 + 1:
-                    break
+				if result == WAIT_OBJECT_0 + 0:
+					# something has changed.  Check all of the files
+					# that match the filter.
+					self.ProcessChangedFiles( )
+					# reset the handle to the change notification and repeat
+					FindNextChangeNotification( hChange )
 
-                # something has changed.  Check all of the files
-                # that match the filter.
-                for file in FindFiles( self.root + self.filter ):
-                    # append the directory to the filename
-                    filepath = self.root + file[8]
-                    last_mod = os.stat( filepath )[ST_MTIME]
-                    # I could have used the following command, but
-                    # it was returning local time, not UTC, so use os.stat
-                    #last_mod = int( file[3] )
-                    if last_mod >= lastCheckedTime:
-                        self.Handle( filepath )
-                    # reset the handle to the change notification and repeat
-                    FindNextChangeNotification( hChange )
-        except:
-            # catch all exceptions and store the information so the calling
-            #  thread can analyze the problem later.
-            self.exception = ( sys.exc_info(), time.gmtime() )
-            apply( traceback.print_exception, self.exception[0] )
-        
-        FindCloseChangeNotification( hChange )        
+				if result == WAIT_OBJECT_0 + 1:
+					break
+
+		except:
+			# catch all exceptions and store the information so the calling
+			#  thread can analyze the problem later.
+			self.exception = ( sys.exc_info(), time.gmtime() )
+			apply( traceback.print_exception, self.exception[0] )
+		
+		FindCloseChangeNotification( hChange )        
+
+	def ProcessChangedFiles( self ):
+		nextCheckTime = time.time()
+		fileSpec = os.path.join( self.root, self.filter )
+		# FindFiles returns tuples... The 9th element is the filename: extract it
+		files = apply( zip, FindFiles( fileSpec ) )[8]
+		# add the path to the filenames
+		files = map( os.path.join, [ self.root ]*len( files ), files )
+		# filter out the ones that haven't changed
+		changed = filter( ChangedSinceFilter( self.lastCheckedTime ), files )
+		# handle the ones that have
+		map( self.Handle, changed )
+		self.lastCheckedTime = nextCheckTime
+
+class StatusHandler( object ):
+	def __init__( self, output = sys.stdout ):
+		self.output = output
+
+	def __call__( self, filename ):
+		self.output.write( '%s changed.\n' % filename )
+		
