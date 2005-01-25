56a6c55d855cd4c38fcd6afbd62a8f9e9dc855ed
jaraco <none@none>
2005-01-25 16:49:33 +0000

    Added copyright notice, docstrings, and utf-8 encoding specifier.
    **********************
    Label: "ICIMOD"
    User: Jaraco       Date: 16-04-04   Time: 11:46
    Labeled
    Label comment: This is the version of the modules as packaged for transfer to ICIMOD.
    
    **********************
    Label: "1.2"
    User: Jaraco       Date: 23-08-02   Time: 17:13
    Labeled
    Label comment: Implements a robust processing service, enhanced SQL object handling, Web, FTP, and File-based data processing.

--- Notifiers.py
+++ Notifiers.py
@@ -1,3 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+"""Notifiers
+	Classes for notifying in the case of an event.
+
+	All objects should support the .write method to append data and
+	.Notify to send the message.
+
+	Objects:
+		SMTPMailbox - sends a message to an SMTP mailbox upon
+			notification.
+			
+Copyright © 2004 Sandia National Laboratories  
+"""
+
+__author__ = 'Jason R. Coombs <jaraco@sandia.gov>'
+__version__ = '$Revision: 3 $a'[11:-2]
+__vssauthor__ = '$Author: Jaraco $'[9:-2]
+__date__ = '$Modtime: 04-06-23 12:27 $'[10:-2]
+
 import string
 import smtplib, socket
 
