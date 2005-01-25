b81f9f7609f7bae9f7ecba02b48918d8814bb916
jaraco <none@none>
2005-01-25 15:02:41 +0000

    I can't tell what changed, but it looks like maybe some line breaks or something got funky.

--- timers.py
+++ timers.py
@@ -3,36 +3,36 @@ import win32event
 import thread
 
 class WaitableTimer:
-    def __init__( self ):
-        self.SignalEvent = win32event.CreateEvent( None, 0, 0, None )
-        self.StopEvent = win32event.CreateEvent( None, 0, 0, None )
+	def __init__( self ):
+		self.SignalEvent = win32event.CreateEvent( None, 0, 0, None )
+		self.StopEvent = win32event.CreateEvent( None, 0, 0, None )
 
-    def Set( self, dueTime, period ):
-        thread.start_new_thread( self._SignalLoop_, ( dueTime, period ) )
+	def Set( self, dueTime, period ):
+		thread.start_new_thread( self._SignalLoop_, ( dueTime, period ) )
 
-    def Stop( self ):
-        win32event.SetEvent( self.StopEvent )
+	def Stop( self ):
+		win32event.SetEvent( self.StopEvent )
 
-    def _SignalLoop_( self, dueTime, period ):
-        if not dueTime and not period:
-            raise ValueError, "dueTime or period must be non-zero"
-        try:
-            if not dueTime:
-                dueTime = time.time() + period
-            if dueTime:
-                self._Wait_( dueTime - time.time() )
-            while period:
-                dueTime += period
-                self._Wait_( dueTime - time.time() ):
-        except Exception:
-            pass
+	def _SignalLoop_( self, dueTime, period ):
+		if not dueTime and not period:
+			raise ValueError, "dueTime or period must be non-zero"
+		try:
+			if not dueTime:
+				dueTime = time.time() + period
+			if dueTime:
+				self._Wait_( dueTime - time.time() )
+			while period:
+				dueTime += period
+				self._Wait_( dueTime - time.time() )
+		except Exception:
+			pass
 
-        #we're done here, just quit
+		#we're done here, just quit
 
-    def _Wait_( self, seconds ):
-        if seconds > 0:
-            res = win32event.WaitForSingleObject( self.StopEvent, seconds )
-            if res == win32event.WAIT_OBJECT_0: raise Exception
-            if res == win32event.WAIT_TIMEOUT: pass
-        win32event.SetEvent( self.SignalEvent )
+	def _Wait_( self, seconds ):
+		if seconds > 0:
+			res = win32event.WaitForSingleObject( self.StopEvent, seconds )
+			if res == win32event.WAIT_OBJECT_0: raise Exception
+			if res == win32event.WAIT_TIMEOUT: pass
+		win32event.SetEvent( self.SignalEvent )
 
