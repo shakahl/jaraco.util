a773c02b3ed9d75c6caad68fa3bcdfe2afa916b3
jaraco <none@none>
2005-01-25 15:02:30 +0000

    Python doesn't support the Win32 Waitable timer, so I created one for myself.

---
+++ timers.py
@@ -0,0 +1,38 @@
+import time
+import win32event
+import thread
+
+class WaitableTimer:
+    def __init__( self ):
+        self.SignalEvent = win32event.CreateEvent( None, 0, 0, None )
+        self.StopEvent = win32event.CreateEvent( None, 0, 0, None )
+
+    def Set( self, dueTime, period ):
+        thread.start_new_thread( self._SignalLoop_, ( dueTime, period ) )
+
+    def Stop( self ):
+        win32event.SetEvent( self.StopEvent )
+
+    def _SignalLoop_( self, dueTime, period ):
+        if not dueTime and not period:
+            raise ValueError, "dueTime or period must be non-zero"
+        try:
+            if not dueTime:
+                dueTime = time.time() + period
+            if dueTime:
+                self._Wait_( dueTime - time.time() )
+            while period:
+                dueTime += period
+                self._Wait_( dueTime - time.time() ):
+        except Exception:
+            pass
+
+        #we're done here, just quit
+
+    def _Wait_( self, seconds ):
+        if seconds > 0:
+            res = win32event.WaitForSingleObject( self.StopEvent, seconds )
+            if res == win32event.WAIT_OBJECT_0: raise Exception
+            if res == win32event.WAIT_TIMEOUT: pass
+        win32event.SetEvent( self.SignalEvent )
+
