a98dfe672fd86cf464c9db093f701a463400f7a3
jaraco <none@none>
2005-01-25 17:03:50 +0000

    Replaced procedural GetUniqueFilaname with functional GetUniquePathname.  The new version supports both filepaths and folder paths.  It no longer requires a root directory to be specified.  GetUniquePathname was also aliased to GetUniqueFilename for backward compatability.

--- fstools.py
+++ fstools.py
@@ -7,24 +7,32 @@ Copyright © 2004 Sandia National Laboratories
 """
 
 __author__ = 'Jason R. Coombs <jaraco@sandia.gov>'
-__version__ = '$Revision: 3 $'[11:-2]
+__version__ = '$Revision: 4 $'[11:-2]
 __vssauthor__ = '$Author: Jaraco $'[9:-2]
-__date__ = '$Modtime: 04-06-23 12:22 $'[10:-2]
+__date__ = '$Modtime: 9-11-04 10:23 $'[10:-2]
 
-import os
+import os, itertools
 
-# return a filename possibly with a number appended to it so that it is
-#  unique in the directory
-def GetUniqueFilename( filename, directory ):
-	basename, ext = os.path.splitext( filename )
-	# save the file with the original name.  If it already exists,
-	#  append a number to the end so as not to overwrite the original
-	number = 0
-	newFilename = filename
-	while 1:
-		destination = os.path.join( directory, newFilename )
-		if not os.path.exists( destination ): break
-		number += 1
-		newFilename = '%s (%d)%s' % ( basename, number, ext )
+def GetUniquePathname( path, root = '' ):
+	"""Return a pathname possibly with a number appended to it so that it is
+	unique in the directory."""
+	path = os.path.join( root, path )
+	# consider the path supplied, then the paths with numbers appended
+	potentialPaths = itertools.chain( ( path, ), __GetNumberedPaths__( path ) )
+	potentialPaths = itertools.ifilterfalse( os.path.exists, potentialPaths )
+	return potentialPaths.next()
 
-	return newFilename
+GetUniqueFilename = GetUniquePathname
+
+def __GetNumberedPaths__( filepath ):
+	"""Append numbers in sequential order to the filename or folder name
+	Numbers should be appended before the extension on a filename."""
+	format = '%s (%%d)%s' % __splitext__( filepath )
+	return itertools.imap( lambda n: format % n, itertools.count( 1 ) )
+
+def __splitext__( filepath ):
+	"Custom version of splitext that doesn't perform splitext on directories"
+	if os.path.isdir( filepath ):
+		return filepath, ''
+	else:
+		return os.path.splitext( filepath )
