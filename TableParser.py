b4963e93bdb17f7c609c806e754c33b71848957d
jaraco <none@none>
2005-01-25 15:55:24 +0000

    Changed table parser to support multiple row spanning cells (so long as they don't alse span columns). Added some logging.
    **********************
    Label: "1.2"
    User: Jaraco       Date: 23-08-02   Time: 17:13
    Labeled
    Label comment: Implements a robust processing service, enhanced SQL object handling, Web, FTP, and File-based data processing.

--- TableParser.py
+++ TableParser.py
@@ -1,7 +1,9 @@
 import htmllib, formatter
+import log
 
 class TableParser( htmllib.HTMLParser ):
 	def __init__( self ):
+		#raise Exception, 'The table parser doesn\'t work right now'
 		htmllib.HTMLParser.__init__( self, formatter.NullFormatter() )
 
 	def feed( self, string ):
@@ -10,23 +12,37 @@ class TableParser( htmllib.HTMLParser ):
 		
 	def start_table( self, attrs ):
 		self.currentTable = []
+		self.extraRows = {}
 
 	def end_table( self ):
 		if self.currentTable:
 			self.tables.append( self.currentTable )
-		del self.currentTable
+		del self.currentTable, self.extraRows
 
 	def start_tr( self, attrs ):
+		# implicitly do </tr> if <tr> is found nested.
+		if hasattr( self, 'currentRow' ):
+			self.end_tr( )
 		self.currentRow = []
+		self.currentColumnNumber = 0
 
 	def end_tr( self ):
-		if self.currentRow:
-			self.currentTable.append( self.currentRow )
-		del self.currentRow
+		if hasattr( self, 'currentRow' ):
+			if self.currentRow:
+				self.currentTable.append( self.currentRow )
+			del self.currentRow, self.currentColumnNumber
+		# else ignore </tr> because no preceding <tr> was found
 
 	def start_td( self, attrs ):
+		self.checkForExtraRows()
+		attrs = dict( attrs )
 		try:
-		    self.extraCols = int(dict(attrs)['colspan']) - 1
+			# TODO: assign for additional columns as well if 'colspan' is set
+			self.extraRows[ self.currentColumnNumber ] = int(attrs['rowspan']) - 1
+		except KeyError:
+			pass
+		try:
+		    self.extraCols = int(attrs['colspan']) - 1
 		except KeyError:
 			self.extraCols = 0
 		# start remembering the contents here
@@ -41,4 +57,17 @@ class TableParser( htmllib.HTMLParser ):
 		except AttributeError:
 			self.currentRow.append( self.save_end() )
 		self.currentRow.extend( [''] * self.extraCols )
+		self.currentColumnNumber += 1
 
+	# note that this will not be called if the extra rows are in the last column(s).
+	def checkForExtraRows( self ):
+		log.processMessage( 'self.extraRows is %s.' % self.extraRows, 'TableParser', log.DEBUG )
+		log.processMessage( 'currentColumnNumber is %s.' % self.currentColumnNumber, 'TableParser', log.DEBUG )
+		if self.extraRows.has_key( self.currentColumnNumber ):
+			self.extraRows[ self.currentColumnNumber ] -= 1
+			self.currentRow.append( '' )
+			if self.extraRows[ self.currentColumnNumber ] == 0:
+				del self.extraRows[ self.currentColumnNumber ]
+			# Now increment the column number and check again
+			self.currentColumnNumber += 1
+			self.checkForExtraRows()
