bd316fc4882ecdbe6c653cb98aa3eb717213b126
jaraco <none@none>
2005-01-25 16:21:56 +0000

    Added a cookie class which will read some cookies from an HTTP response header and will output their value for an HTTP request header.  It does not handle domain or path validation.

--- cookies.py
+++ cookies.py
@@ -1,8 +1,9 @@
 import os, copy
 import itertools
-import string
+import string, re
 
 class CookieMonster( object ):
+	"Read cookies out of a user's IE cookies file"
 	def __init__( self, filename ):
 		profileDir = os.environ['USERPROFILE']
 		self.cookieDir = os.path.join( profileDir, 'Cookies' )
@@ -17,11 +18,13 @@ class CookieMonster( object ):
 			cookie = self.makeCookie( *entry )
 			yield cookie
 
-	def makeCookie( self, Key, Value, Domain, Flags, ExpireLow, ExpireHigh, CreateLow, CreateHigh ):
-		Expires = ( int( ExpireHigh ) << 32 ) | int( ExpireLow )
-		Created = ( int( CreateHigh ) << 32 ) | int( CreateLow )
+	def makeCookie( self, key, value, domain, flags, ExpireLow, ExpireHigh, CreateLow, CreateHigh ):
+		expires = ( int( ExpireHigh ) << 32 ) | int( ExpireLow )
+		created = ( int( CreateHigh ) << 32 ) | int( CreateLow )
 		del ExpireHigh, ExpireLow, CreateHigh, CreateLow
-		Flags = int( Flags )
+		flags = int( flags )
+		domain, path = string.split( domain, '/', 1 )
+		path = '/' + path
 		cookie = vars()
 		del cookie['self']
 		return cookie
@@ -29,3 +32,33 @@ class CookieMonster( object ):
 
 def isNotCookieDelimiter(s ):
 	return s != '*\n'
+
+class cookie( dict ):
+	parameterNames = ( 'expires', 'path', 'domain', 'secure' )
+	def __init__( self, header = None ):
+		if header:
+			self.readFromSetHeader( header )
+			
+	def readFromSetHeader( self, header ):
+		fields = re.split( ';\s*', header )
+		splitEquals = lambda x: x.split( '=' )
+		fieldPairs = map( splitEquals, fields )
+		self.update( dict( fieldPairs ) )
+		self.findName()
+
+	def findName( self ):
+		"Find the name of the cookie, which should be the only pair that's not a parameter"
+		isNotParameter = lambda k: k not in self.parameterNames
+		names = filter( isNotParameter, self )
+		if not len( names ) == 1:
+			raise ValueError, "Found more than one name/value pair where name isn't a cookie parameter"
+		name = names[0]
+		self['name'] = name
+		self['value'] = self[name]
+		del self[name]
+
+	def getRequestHeader( self ):
+		return '='.join( ( self['name'], self['value'] ) )
+
+	def isSecure( self ):
+		return eval( string.capwords( self.get( 'secure', 'False' ) ) )
