d267da0c7c3d52116f8314d4d19ced423ff3bdf8
jaraco <none@none>
2005-01-25 16:34:45 +0000

    Tunnel proxy support (for HTTPS through a proxy).  ********************** Label: "1.2" User: Jaraco       Date: 23-08-02   Time: 17:13 Labeled Label comment: Implements a robust processing service, enhanced SQL object handling, Web, FTP, and File-based data processing.

---
+++ ProxyTunnel.py
@@ -0,0 +1,54 @@
+# stuff
+
+import httplib, urllib2, urllib
+import autohttp
+
+class TunnelResponse( httplib.HTTPResponse ):
+	def _check_close( self ):
+		# assume it will not close
+		# I don't know under what conditions the proxy will deny a connection.
+		# It seems the Sandia proxy always allows the connection.
+		return False
+
+	def begin( self ):
+		# HTTPResponse always assumes the connection will close unless
+		#  the response is chunked or length is not None, both of which are
+		#  not true in the tunnel case... so override the will_close value.
+		httplib.HTTPResponse.begin( self )
+		self.will_close = False
+
+class HTTPSConnectionEstablished( httplib.HTTPSConnection ):
+	"""HTTPS Connection class, identical to httplib version except an already
+	established link may be used."""
+	def connect( self, sock = None ):
+		if not sock:
+			httplib.HTTPSConnection.connect( self )
+		else:
+			ssl = httplib.socket.ssl( sock, self.key_file, self.cert_file )
+			self.sock = httplib.FakeSocket( sock, ssl )
+
+class HTTPSHandler( autohttp.HTTPSHandler ):
+	def https_open( self, req ):
+		return self.do_open( HTTPSConnectionEstablished, req )
+
+class ProxyTunnelHandler( urllib2.ProxyHandler ):
+	def https_open( self, request ):
+		proxy = urllib2.urlparse.urlparse( self.proxies['http'] )[1]
+		tunnel_connection = httplib.HTTPConnection( proxy )
+		tunnel_connection.response_class = TunnelResponse
+		host_port = urllib.splitnport( request.get_host(), httplib.HTTPS_PORT )
+		tunnel_connection.putrequest( 'CONNECT', '%s:%d' % host_port )
+		tunnel_connection.endheaders()
+		response = tunnel_connection.getresponse()
+		response.read()
+		request.sock = tunnel_connection.sock
+		
+		# return None so other handlers will handle the modified request
+
+def test():
+	o = urllib2.build_opener( ProxyTunnelHandler( {'http':'http://sonproxy.sandia.gov:80'} ),
+							  HTTPSHandler, autohttp.HTTPHandler ) 
+	urllib2.install_opener( o )
+	return urllib2.urlopen( 'https://www.jaraco.com:444/' )
+	
+	
