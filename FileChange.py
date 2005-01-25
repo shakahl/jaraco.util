fe965265340642e838c2aab17ea431d9f876d98c
jaraco <none@none>
2005-01-25 16:06:52 +0000

    Removed legacy objects ChangedSinceFilter.  Move functionality of FileChangeNotifier into ThreadedNotifier.  Renamed StatusHandler to StreamHandler and FileChangeNotifierException to NotifierException.
    Fixed a bug where recursive directories would not be handled properly since files and pathnames were concatenanted too early.
    
    Fixed change notification to properly watch subtree for changes when the watchSubtree attribute has been set to true.
    
    Moved WaitResults into BlockingNotifier.

--- FileChange.py
+++ FileChange.py
@@ -5,7 +5,7 @@ from stat import *
 import itertools
 
 import logging
-log = logging.getLogger( 'FileChangeNotifier' )
+log = logging.getLogger( __name__ )
 
 # win32* requires ActivePython by Mark Hammond (thanks Mark!)
 from win32file import *
@@ -13,130 +13,18 @@ from win32api import *
 from win32event import *
 from win32con import FILE_NOTIFY_CHANGE_LAST_WRITE
 
-
-class ChangedSinceFilter( object ):
-	def __init__( self, cutoff ):
-		# truncate the time to the second.
-		self.cutoff = int( cutoff )
-
-	def __call__( self, filepath ):
-		last_mod = os.stat( filepath )[ST_MTIME]
-		log.debug( '%s last modified at %s.', filepath, time.asctime( time.localtime( last_mod ) ) )
-		return last_mod >= self.cutoff
-
-class FileChangeNotifierException( Exception ):
+class NotifierException( Exception ):
 	pass
-
-# FileChangeNotifier provides a simple interface that calls the handler
-#  for each file that is updated matching root\filter
-# Note: files are only checked in the root directory (subdirectories ignored)
-# Note: files that are updated while the handler is processing are not caught
-
-class FileChangeNotifier( Thread ):
-	def __init__( self, root = '.', filter = '*', handler = lambda file: None ):
-		# init thread stuff
-		Thread.__init__( self )
-		# set it as a daemon thread so that it doesn't block waiting to close.
-		#  I tried setting __del__( self ) to .Quit(), but unfortunately, there are
-		#  references to this object in the win32api stuff, so __del__ never gets
-		#  called.
-		self.setDaemon( 1 )
-		
-		# assign the root, verify it exists
-		self.root = root
-		if not os.path.isdir( self.root ):
-			raise FileChangeNotifierException( 'Root directory %s does not exist' % self.root )
-
-		# assign the filter and handler
-		self.filter = filter
-		self.Handle = handler
-
-		self.QuitEvent = CreateEvent( None, 0, 0, None )
-
-		self.watchSubtree = 0		
-
-	def Quit( self ):
-		SetEvent( self.QuitEvent )
-
-	def run( self ):
-		# set up to monitor the directory tree specified
-		hChange = FindFirstChangeNotification( self.root, 0, \
-											   FILE_NOTIFY_CHANGE_LAST_WRITE )
-
-		# make sure it worked; if not, bail
-		if hChange == INVALID_HANDLE_VALUE:
-			raise FileChangeNotifierException, 'Could not set up directory change notification'
-
-		# continuously monitor files and handle them if they've changed
-		try:
-			# make account of when we started waiting for updates
-			self.lastCheckedTime = time.time()
-			while (1):
-				# block (sleep) until something changes in the
-				#  target directory or a quit is requested.
-				# timeout so we can catch keyboard interrupts or other exceptions
-				result = WAIT_TIMEOUT
-				while result == WAIT_TIMEOUT:
-					result = WaitForMultipleObjects( ( hChange, self.QuitEvent ), 0, 1000 )
-
-				if result == WAIT_OBJECT_0 + 0:
-					# something has changed.  Check all of the files
-					# that match the filter.
-					nextCheckTime = time.time()
-					log.debug( 'Looking for all files changed after %s', time.asctime( time.localtime( self.lastCheckedTime ) )  )
-					self.ProcessChangedFiles( )
-					self.lastCheckedTime = nextCheckTime
-					# reset the handle to the change notification and repeat
-					FindNextChangeNotification( hChange )
-
-				if result == WAIT_OBJECT_0 + 1:
-					break
-
-		except:
-			# catch all exceptions and store the information so the calling
-			#  thread can analyze the problem later.
-			self.exception = ( sys.exc_info(), time.gmtime() )
-			traceback.print_exc()
-		
-		FindCloseChangeNotification( hChange )        
-
-	def ProcessChangedFiles( self, path = None ):
-		if not path:
-			path = self.root
-		fileSpec = os.path.join( path, self.filter )
-		log.debug( 'Looking for changed files matching %s', fileSpec )
-		files = FindFiles( fileSpec )
-		if files:
-			# FindFiles returns tuples... The 9th element is the filename: extract it
-			files = zip( *files )[8]
-		# create a function for prepending the path
-		prepender = lambda x: os.path.join( path, x )
-		# add the path to the filenames
-		files = map( prepender, files )
-		# filter out the ones that haven't changed
-		changed = filter( ChangedSinceFilter( self.lastCheckedTime ), files )
-		log.debug( 'These files have changed: %s', changed )
-		# handle the ones that have
-		map( self.Handle, changed )
-		if self.watchSubtree:
-			directories = map( prepender, os.listdir( path ) )
-			directories = filter( os.path.isdir, directories )
-			map( self.ProcessChangedFiles, directories )
-
-	def WatchSubtree( self, choice = 1 ):
-		self.watchSubtree = choice
-
-# the status handler is a sample handler object that will
-#  announce that a file has changed.
-class StatusHandler( object ):
-	def __init__( self, output = sys.stdout ):
-		self.output = output
-
-	def __call__( self, filename ):
-		self.output.write( '%s changed.\n' % filename )
 
 class FileFilter( object ):
-	pass
+	def SetRoot( self, root ):
+		self.root = root
+
+	def _GetFilePath( self, filename ):
+		try:
+			filename = os.path.join( self.root, filename )
+		except AttributeError: pass
+		return filename
 
 class ModifiedTimeFilter( FileFilter ):
 	""" Returns true for each call where the modified time of the file is after the cutoff time """
@@ -144,19 +32,27 @@ class ModifiedTimeFilter( FileFilter ):
 		# truncate the time to the second.
 		self.cutoff = int( cutoff )
 
-	def __call__( self, filepath ):
+	def __call__( self, file ):
+		filepath = self._GetFilePath( file )
 		last_mod = os.stat( filepath ).st_mtime
 		log.debug( '%s last modified at %s.', filepath, time.asctime( time.localtime( last_mod ) ) )
 		return last_mod >= self.cutoff
 
 class PatternFilter( FileFilter ):
+	"""
+	This file filter when called will return True for those files that match the pattern.
+	  When initialized, exactly one of filePattern or rePattern must be supplied.
+
+	filePattern is a DOS-like file name with wildcards (*,?)
+	rePattern is a regular expression.
+	"""
 	def __init__( self, filePattern = None, rePattern = None ):
 		if filePattern and rePattern:
 			raise TypeError, 'PatternFilter() takes exactly 1 argument (2 given).'
 		if not filePattern and not rePattern:
 			raise TypeError, 'PatternFilter() takes exactly 1 argument (0 given).'
 		if filePattern:
-			self.pattern = self.ConvertFilePattern( filePattern )
+			self.pattern = PatternFilter.ConvertFilePattern( filePattern )
 		if rePattern:
 			self.pattern = rePattern
 
@@ -173,18 +69,24 @@ class PatternFilter( FileFilter ):
 		return p
 	ConvertFilePattern = staticmethod( ConvertFilePattern )
 
-	def __call__( self, filepath ):
-		filename = os.path.basename( filepath )
-		return operator.truth( re.match( self.pattern, filename ) )
+	def __call__( self, file ):
+		return operator.truth( re.match( self.pattern, file ) )
 
 class AggregateFilter( FileFilter ):
+	"""
+	This file filter will aggregate the filters passed to it, and when called, will
+	  return the results of each filter ANDed together.
+	"""
 	def __init__( self, *filters ):
 		self.filters = filters
 
-	def __call__( self, filepath ):
-		results = map( lambda x: x( filepath ), self.filters )
+	def SetRoot( self, root ):
+		map( lambda x: x.SetRoot( root ), self.filters )
+
+	def __call__( self, file ):
+		results = map( lambda x: x( file ), self.filters )
 		result = reduce( operator.and_, results )
-		return result
+		return result	
 
 def filesWithPath( files, path ):
 	for file in files:
@@ -212,8 +114,11 @@ class Notifier( object ):
 
 	def _GetChangeHandle( self ):
 		# set up to monitor the directory tree specified
-		self.hChange = FindFirstChangeNotification( self.root, 0, \
-											   FILE_NOTIFY_CHANGE_LAST_WRITE )
+		self.hChange = FindFirstChangeNotification(
+			self.root,
+			self.watchSubtree,
+			FILE_NOTIFY_CHANGE_LAST_WRITE
+		)
 
 		# make sure it worked; if not, bail
 		if self.hChange == INVALID_HANDLE_VALUE:
@@ -224,7 +129,8 @@ class Notifier( object ):
 		for root, dirs, files in os.walk( path ):
 			log.debug( 'looking in %s', root )
 			log.debug( 'files is %s', files )
-			files = filter( fileFilter, filesWithPath( files, root ) )
+			fileFilter.SetRoot( root )
+			files = filter( fileFilter, files )
 			log.debug( 'filtered files is %s', files )
 			yield ( root, dirs, files )
 	_FilteredWalk = staticmethod( _FilteredWalk )
@@ -232,15 +138,12 @@ class Notifier( object ):
 	def Quit( self ):
 		SetEvent( self.QuitEvent )
 
-class ThreadedNotifier( Notifier ):
-	"""This class will replace FileChangeNotifier above"""
-	pass
-
-def WaitResults( *args ):
-	""" calls WaitForMultipleObjects repeatedly with args """
-	return itertools.starmap( WaitForMultipleObjects, itertools.repeat( args ) )
-
 class BlockingNotifier( Notifier ):
+
+	def WaitResults( *args ):
+		""" calls WaitForMultipleObjects repeatedly with args """
+		return itertools.starmap( WaitForMultipleObjects, itertools.repeat( args ) )
+	WaitResults = staticmethod( WaitResults )
 
 	def GetChangedFiles( self ):
 		self._GetChangeHandle()
@@ -248,7 +151,7 @@ class BlockingNotifier( Notifier ):
 		# block (sleep) until something changes in the
 		#  target directory or a quit is requested.
 		# timeout so we can catch keyboard interrupts or other exceptions
-		for result in WaitResults( ( self.hChange, self.QuitEvent ), False, 1000 ):
+		for result in BlockingNotifier.WaitResults( ( self.hChange, self.QuitEvent ), False, 1000 ):
 			if result == WAIT_OBJECT_0 + 0:
 				# something has changed.
 				log.debug( 'Change notification received' )
@@ -275,3 +178,41 @@ class BlockingNotifier( Notifier ):
 			result = results.next()
 		return result
 
+class ThreadedNotifier( BlockingNotifier, Thread ):
+	"""
+	ThreadedNotifier provides a simple interface that calls the handler
+		for each file rooted in root that passes the filters.  It runs as its own
+		thread, so must be started as such.
+
+	>>> notifier = FileChange.ThreadedNotifier( 'c:\\', handler = FileChange.StreamHandler() )
+	>>> n.start()
+	>>> C:\Autoexec.bat changed.
+	"""
+	def __init__( self, root = '.', filters = [], handler = lambda file: None ):
+		# init notifier stuff
+		BlockingNotifier.__init__( self, root, filters )
+		# init thread stuff
+		Thread.__init__( self )
+		# set it as a daemon thread so that it doesn't block waiting to close.
+		#  I tried setting __del__( self ) to .Quit(), but unfortunately, there are
+		#  references to this object in the win32api stuff, so __del__ never gets
+		#  called.
+		self.setDaemon( True )
+		
+		self.Handle = handler
+
+	def run( self ):
+		for file in self.GetChangedFiles():
+			self.Handle( file )
+
+class StreamHandler( object ):
+	"""
+	StreamHandler: a sample handler object for use with the threaded notifier
+		that will announce by writing to the supplied stream (stdout by default) the
+		name of the file.
+	"""
+	def __init__( self, output = sys.stdout ):
+		self.output = output
+
+	def __call__( self, filename ):
+		self.output.write( '%s changed.\n' % filename )
