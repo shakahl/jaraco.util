80f844fb0bbfb64eaf9423c201d539a93a75a170
SANDIA\jaraco <none@none>
2005-06-21 14:43:09 +0000

    Added support for .ar domains.

--- dotgovwhois.py
+++ dotgovwhois.py
@@ -21,7 +21,7 @@ __svnauthor__ = '$Author:  $'[9:-2]
 __date__ = '$Date:  $'[7:-2]
 
 import cookielib, urllib2, os, re
-from ClientForm import ParseResponse
+from ClientForm import ParseResponse, ItemNotFoundError
 
 def init( ):
 	"""Initialize HTTP functionality to support cookies, which are necessary
@@ -30,7 +30,27 @@ def init( ):
 	opener = urllib2.build_opener( urllib2.HTTPCookieProcessor(cj) )
 	urllib2.install_opener( opener )
 
-def doQuery( query ):
+def GetFunctions( query ):
+	if re.search( r'\.ar$', query ): return doARQuery, ARParser
+	if re.search( r'(\.fed\.us|\.gov)$', query ): return doGovQuery, GovParser
+
+argentina = ( '.com.ar', '.gov.ar', '.int.ar', '.mil.ar', '.net.ar', '.org.ar' )
+def doARQuery( query ):
+	pageURL = 'http://www.nic.ar/consdom.html'
+	form = ParseResponse( urllib2.urlopen( pageURL ) )[0]
+	form['nombre'] = query[ :query.find( '.' ) ]
+	try:
+		domain = query[ query.find( '.' ) : ]
+		form['dominio'] = [ domain ]
+	except ItemNotFoundError:
+		raise ValueError, 'Invalid domain (%s)' % domain
+	req = form.click()
+	#req.data = 'nombre=%s&dominio=.com.ar' % query
+	req.add_header( 'referer', pageURL )
+	resp = urllib2.urlopen( req )
+	return resp.read()
+
+def doGovQuery( query ):
 	"Perform an whois query on the dotgov server."
 	url = urllib2.urlopen( 'http://dotgov.gov/whois.aspx' )
 	forms = ParseResponse( url )
@@ -43,7 +63,7 @@ def doQuery( query ):
 		# note this could get to an infinite loop if cookies aren't working
 		# or for whatever reason we're always being redirected to the
 		# agree.aspx page.
-		return doQuery( query )
+		return doGovQuery( query )
 	form['who_search'] = query
 	resp = urllib2.urlopen( forms[0].click() )
 	return resp.read()
@@ -56,9 +76,9 @@ def Agree( form ):
 
 from htmllib import HTMLParser
 from formatter import NullFormatter, DumbWriter, AbstractFormatter
-class dotgovparser( HTMLParser ):
-	def __init__( self, writer ):
-		self.__formatter__ = AbstractFormatter( writer )
+class GovParser( HTMLParser ):
+	def __init__( self, formatter ):
+		self.__formatter__ = formatter
 		# Use the null formatter to start; we'll switch to the outputting
 		#  formatter when we find the right point in the HTML.
 		HTMLParser.__init__( self, NullFormatter() )
@@ -75,7 +95,13 @@ class dotgovparser( HTMLParser ):
 		# switch back to the NullFormatter
 		if not isinstance( self.formatter, NullFormatter ):
 			self.formatter = NullFormatter( )
-	
+
+class ARParser( HTMLParser ):
+	def start_tr( self, attrs ):
+		pass # have to define this for end_tr to be called.
+		
+	def end_tr( self ):
+		self.formatter.add_line_break()
 
 class MyWriter( DumbWriter ):
 	def send_flowing_data( self, data ):
@@ -89,17 +115,25 @@ class Handler( StreamRequestHandler ):
 	def handle( self ):
 		query = self.rfile.readline().strip()
 		try:
-			response = doQuery( query )
+			queryFunc, parser = GetFunctions( query )
+		except TypeError:
+			self.wfile.write( 'Domain for %s not serviced by this server.\n' % query )
+			return
+		try:
+			response = queryFunc( query )
 		except urllib2.URLError:
 			self.wfile.write( 'Could not contact whois HTTP service.\n' )
 			return
+		except ValueError, e:
+			self.wfile.write( '%s\n' % e )
+			return
 		# fix the response; the parser doesn't understand tags that have a slash
-		# immediately following the tag name
+		# immediately following the tag name (part of the XHTML 1.0 spec).
 		# Alternatively, one could use tidylib with 'drop-empty-paras' set to False
 		# response = str( tidy.parseString( response, drop_empty_paras = False ) )
 		response = re.sub( r'<(\w+)/>', r'<\1 />', response )
 		writer = MyWriter( self.wfile )
-		dotgovparser( writer ).feed( response )
+		parser( AbstractFormatter( writer ) ).feed( response )
 
 class Listener( TCPServer, ThreadingMixIn ):
 	def __init__( self ):
