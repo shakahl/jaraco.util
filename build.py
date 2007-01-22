86d718fb36ab308fb7abcd443379e8f26cc9d370
JARACO\jaraco <none@none>
2007-01-22 17:25:15 +0000

    Added tuple initialization support to odict Updated to version 1.0.3

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 
 """ Setup script for building General Purpose Module distribution
 
-Copyright © 2004 Sandia National Laboratories
+Copyright © 2004-2006 Jason R. Coombs
 """
 
 from distutils.core import setup
@@ -13,7 +13,7 @@ __svnauthor__ = '$Author$'[9:-2]
 __date__ = '$Date$'[7:-2]
 
 setup ( name = 'GeneralPurpose',
-		version = '1.0.2',
+		version = '1.0.3',
 		description = 'General Purpose Modules',
 		author = 'Jason R. Coombs',
 		author_email = 'jaraco@sandia.gov',
