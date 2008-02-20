70e64e1c72da08477681888b1df0a5b6f251e87d
jaraco <none@none>
2008-02-20 15:24:42 +0000

    Renamed project to jaraco-util. Modified project to use setuptools. Updated version to 2.1.

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+
+""" Setup script for building General Purpose Module distribution
+
+Copyright © 2004-2008 Jason R. Coombs
+"""
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+__author__ = 'Jason R. Coombs <jaraco@sandia.gov>'
+__version__ = '$Rev$'[6:-2]
+__svnauthor__ = '$Author$'[9:-2]
+__date__ = '$Date$'[7:-2]
+
+setup ( name = 'jaraco-util',
+		version = '2.1',
+		description = 'General utility modules to support functionality that Python maybe should support natively',
+		author = 'Jason R. Coombs',
+		author_email = 'jaraco@jaraco.com',
+		url = 'http://www.jaraco.com/',
+		packages = [''],
+		package_dir = { '':'lib' },
+		license = 'MIT',
+		classifiers = [
+			"Development Status :: 4 - Beta",
+			"Intended Audience :: Developers",
+			"Programming Language :: Python",
+		],
+		)
