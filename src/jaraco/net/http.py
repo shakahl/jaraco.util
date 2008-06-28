3a9217b5e05c4d9e855e56b40c6b6b94b3b2aba2
jaraco <none@none>
2008-06-28 19:04:14 +0000

    Brought back PageGetter from r487 /Modules/webtools.py.

--- src/jaraco/net/http.py
+++ src/jaraco/net/http.py
@@ -2,7 +2,11 @@ import socket, sys, re, time
 from optparse import OptionParser
 import urlparse
 import urllib
+import urllib2
+import ClientForm
+import cookielib
 from jaraco.util import splitter
+
 
 p = OptionParser()
 p.add_option('-p', '--port', type='int', help="Bind to port", default=80)
@@ -96,4 +100,50 @@ class Query(dict):
 	def __QueryFromURL__(url):
 		"Return the query portion of a URL"
 		return urlparse.urlparse(url)[4]
+
+class PageGetter(object):
+	"""
+	PageGetter
+	A helper class for common HTTP page retrieval.
+	"""
+	
+	_opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+	
+	def __init__(self, **attrs):
+		"set url to the target url or set request to the urllib2.Request object"
+		self.__dict__.update(attrs)
+
+	def GetRequest(self):
+		req = getattr(self, 'request', None) or urllib2.Request(getattr(self, 'url'))
+		return req
+
+	def Fetch(self):
+		return self._opener.open(self.GetRequest())
+
+	def Process(self):
+		resp = self.Fetch()
+		forms = ClientForm.ParseResponse(resp)
+		form = self.SelectForm(forms)
+		self.FillForm(form)
+		return form.click()
+
+	def SelectForm(self, forms):
+		sel = getattr(self, 'form_selector', 0)
+		log.info('selecting form %s', sel)
+		if not isinstance(sel, int):
+			# assume the selector is the name of the form
+			forms = dict(map(lambda f: (f.name, f), forms))
+		return forms[sel]
+
+	def FillForm(self, form):
+		for name, value in self.form_items.items():
+			if callable(value):
+				value = value()
+			form[name] = value
+
+	def __call__(self, next):
+		# process the form and set the request for the next object
+		next.request = self.Process()
+		return next
+
 
