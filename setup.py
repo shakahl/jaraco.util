46bb350a2d9ca1171ad5fc0e50bdf14e1f177b29
jaraco <none@none>
2009-02-18 22:51:21 +0000

    Moved .net and .media into their own projects

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 """ Setup script for building jaraco-util distribution
 
-Copyright © 2004-2008 Jason R. Coombs
+Copyright © 2004-2009 Jason R. Coombs
 """
 
 from ez_setup import use_setuptools
@@ -31,33 +31,16 @@ setup (name = 'jaraco.util',
 		],
 		entry_points = {
 			'console_scripts': [
-				'whois_bridge = jaraco.net.whois:main',
-				'scanner = jaraco.net.scanner:scan',
-				'fake-http = jaraco.net.http:start_simple_server',
-				'wget = jaraco.net.http:wget',
-				'fake-smtp = jaraco.net.smtp:start_simple_server',
-				'udp-send = jaraco.net.udp:Sender',
-				'udp-echo = jaraco.net.udp:EchoServer',
-				'dns-forward-service = jaraco.net.dns:start_service',
 				'roll-dice = jaraco.util.dice:do_dice_roll',
-				'encode-dvd = jaraco.media.dvd:encode_dvd',
-				'crop-detect = jaraco.media.cropdetect:execute',
-				'dvd-info = jaraco.media.dvd_info:main',
-				'dnsbl-check = jaraco.net.dnsbl:handle_cmdline',
 				],
 		},
 		install_requires=[
-			'clientform>=0.2.7',
-			'BeautifulSoup',
 		],
 		extras_require = {
 			'image':
 				['pil>=1.1.6'],
-#			'what_requires_this?': 
-#				['pyxml>=0.8.4'],
 		},
 		dependency_links = [
-			"http://www.jaraco.com/ASP/eggs",
 		],
 		tests_require=[
 			'nose>=0.10',
