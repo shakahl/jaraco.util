d307b2bc0413e62aac1be465b1fbf3473e56c16a
jaraco <none@none>
2009-03-15 16:16:25 +0000

    Fixed issue with read_long_description

--- jaraco/util/package.py
+++ jaraco/util/package.py
@@ -5,7 +5,7 @@ import os
 def read_long_description(setup_file):
 	return open(
 		os.path.join(
-			os.path.dirname(__file__),
+			os.path.dirname(setup_file),
 			'docs',
 			'index.txt',
 		) ).read().strip()
