1134e48df8bb98d135138bf4f15ca3485ccd6c0c
jaraco <none@none>
2009-08-25 23:24:56 +0000

    Moved PasswordGenerator from jaraco.site

--- jaraco/util/__init__.py
+++ jaraco/util/__init__.py
@@ -461,4 +461,33 @@ def ordinalth(n):
 	index = [ones, 0][forceth]
 	return '%d%s' % (n, t[index])
 
-	
+import codecs, random, itertools
+
+class PasswordGenerator(object):
+	"""
+	Generates random passwords
+	>>> pw = PasswordGenerator.make_password(8, encoding=None)
+	>>> len(pw)
+	8
+	>>> pw != PasswordGenerator.make_password(8, encoding=None)
+	True
+	"""
+
+	@staticmethod
+	def make_password(n_bytes = 8, encoding = 'base-64'):
+		'Make a password with n_bytes of disorder; optionally encoded'
+		chars = PasswordGenerator.get_random_chars(n_bytes)
+		result = ''.join(chars)
+		null_encoder = lambda s: (s, len(s))
+		encoder = codecs.getencoder(encoding) if encoding else null_encoder
+		encoded, length = encoder(result)
+		return encoded
+
+	@staticmethod
+	def get_random_chars(len):
+		return itertools.islice(PasswordGenerator.random_byte_generator(), len)
+
+	@staticmethod
+	def random_byte_generator():
+		while True:
+			yield chr(random.randint(0, 255))
