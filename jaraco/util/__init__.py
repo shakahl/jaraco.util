12d84857a78138644d1950447ed5436fdff57889
jaraco <none@none>
2009-09-20 22:22:14 +0000

    moved ciString to string.FoldedCase

--- jaraco/util/__init__.py
+++ jaraco/util/__init__.py
@@ -225,47 +225,8 @@ class splitter(object):
 	def __call__(self, s):
 		return s.split(*self.args)
 
-class ciString(str):
-	"""
-	A case insensitive string class; behaves just like str
-	except compares equal when the only variation is case.
-	>>> s = ciString('hello world')
-
-	>>> s == 'Hello World'
-	True
-
-	>>> 'Hello World' == s
-	True
-
-	>>> s.index('O')
-	4
-
-	>>> s.split('O')
-	['hell', ' w', 'rld']
-
-	>>> sorted(map(ciString, ['GAMMA', 'alpha', 'Beta']))
-	['alpha', 'Beta', 'GAMMA']
-	"""
-	def __lt__(self, other):
-		return self.lower() < other.lower()
-	def __gt__(self, other):
-		return self.lower() > other.lower()
-	def __eq__(self, other):
-		return self.lower() == other.lower()
-	def __hash__(self):
-		return hash(self.lower())
-	# cache lower since it's likely to be called frequently.
-	def lower(self):
-		self._lower = super(ciString, self).lower()
-		self.lower = lambda: self._lower
-		return self._lower
-
-	def index(self, sub):
-		return self.lower().index(sub.lower())
-
-	def split(self, splitter=' ', maxsplit=0):
-		pattern = re.compile(re.escape(splitter), re.I)
-		return pattern.split(self, maxsplit)
+# kept for backward compatibility
+from .string import FoldedCase as ciString
 
 class ciDict(dict):
 	"""A case-insensitive dictionary (keys are compared as insensitive
