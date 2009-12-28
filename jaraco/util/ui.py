a47f963106878d9e6cb76aab8e838cf6d4e12a1a
jaraco <none@none>
2009-12-28 22:19:21 +0000

    Moved menu from script where it was being used

---
+++ jaraco/util/ui.py
@@ -0,0 +1,28 @@
+from itertools import count
+
+class Menu(object):
+	"""
+	A simple command-line based menu
+	"""
+	def __init__(self, choices=None):
+		self.choices = choices or list()
+
+	def get_choice(self, prompt="> "):
+		number_width = len(self.choices)/10+1
+		menu_fmt = '{number:{number_width}}) {choice}'
+		for number, choice in zip(count(1), self.choices):
+			print(menu_fmt.format(**vars()))
+		print()
+		try:
+			answer = int(raw_input(prompt))
+			result = self.choices[answer-1]
+		except ValueError:
+			print('invalid selection')
+			result = None
+		except IndexError:
+			print('invalid selection')
+			result = None
+		except KeyboardInterrupt:
+			result = None
+		return result
+
