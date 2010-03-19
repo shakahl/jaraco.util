f99cdfa9ff8283679b33864c8e8be0bbeb08cef4
jaraco <none@none>
2010-03-19 16:11:45 +0000

    Added support for different sided dice

--- jaraco/util/dice.py
+++ jaraco/util/dice.py
@@ -2,18 +2,34 @@ from __future__ import print_function
 
 import random
 import sys
+from optparse import OptionParser
+
+def get_options():
+	parser = OptionParser()
+	parser.add_option('-s', '--sides', default=6, type="int")
+	options, args = parser.parse_args()
+	try:
+		number = int(args.pop())
+	except:
+		number = 1
+	options.number = number
+	return options
+
+class Dice(object):
+	def __init__(self, sides):
+		self.sides = sides
+
+	def roll(self):
+		return random.randint(1, self.sides)
 
 def do_dice_roll():
 	"""
-	Roll n 6-sided dice and return each result and the total
+	Roll n-sided dice and return each result and the total
 	"""
-	try:
-		n_dice = int(sys.argv[1])
-	except:
-		n_dice = 1
-	total = 0
-	for dice in range(n_dice):
-		value = random.randint(1,6)
-		total += value
-		print('rolled', value)
-	if n_dice > 1: print('total', total)
+	options = get_options()
+	dice = Dice(options.sides)
+	rolls = [dice.roll() for n in range(options.number)]
+	for roll in rolls:
+		print('rolled', roll)
+	if options.number > 1:
+		print('total', sum(rolls))
