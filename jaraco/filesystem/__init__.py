7f32abcfae63b3d8c2cfcf93f1d4fc7771c141d1
jaraco <none@none>
2010-05-01 13:54:55 +0000

    Added function to easily insert text before the extension

--- jaraco/filesystem/__init__.py
+++ jaraco/filesystem/__init__.py
@@ -62,3 +62,15 @@ def get_time(filename):
 	"""
 	ts = os.stat(filename).st_mtime
 	return datetime.datetime.utcfromtimestamp(ts)
+
+def insert_before_extension(filename, content):
+	"""
+	Given a filename and some content, insert the content just before
+	the extension.
+	
+	>>> insert_before_extension('pages.pdf', '-old')
+	'pages-old.pdf'
+	"""
+	parts = list(os.path.splitext(filename))
+	parts[1:1] = [content]
+	return ''.join(parts)
