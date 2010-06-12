d1cf59ae569d5828aa46571c081f3e5f0d39d33b
jaraco <none@none>
2010-06-12 05:41:54 +0000

    Added re-usable set_connection_environment function.

--- jaraco/util/turk.py
+++ jaraco/util/turk.py
@@ -35,12 +35,25 @@ def make_turk_recognition_job_from_pdf():
 		hostname = socket.getfqdn()
 		print(lf('http://{hostname}/pages/{file}'), file=job)
 
-def get_connection(access_key='0ZWJV1BMM1Q6GXJ9J2G2'):
-	from boto.mturk.connection import MTurkConnection
+def set_connection_environment(access_key='0ZWJV1BMM1Q6GXJ9J2G2'):
+	"""
+	boto requires the credentials to be either passed to the connection,
+	stored in a unix-like config file unencrypted, or available in
+	the environment, so pull the encrypted key out and put it in the
+	environment.
+	"""
 	import keyring
 	secret_key = keyring.get_password('AWS', access_key)
-	return MTurkConnection(access_key, secret_key, is_secure=True,
-		host='mechanicalturk.sandbox.amazonaws.com', debug=True)
+	os.environ['AWS_ACCESS_KEY_ID'] = access_key
+	os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
+
+def get_connection():
+	from boto.mturk.connection import MTurkConnection
+	set_connection_environment()
+	return MTurkConnection(
+		host='mechanicalturk.sandbox.amazonaws.com',
+		debug=True,
+		)
 
 def get_question():
 	from boto.mturk.question import Question, FreeTextAnswer, QuestionContent
