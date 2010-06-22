3cc1ce2e97a1a133969b9212882ce34b552322f9
jaraco <none@none>
2010-06-22 03:25:28 +0000

    Converted most of HTML into turk API (not yet working)

--- jaraco/util/turk.py
+++ jaraco/util/turk.py
@@ -35,6 +35,10 @@ def make_turk_recognition_job_from_pdf():
 		hostname = socket.getfqdn()
 		print(lf('http://{hostname}/pages/{file}'), file=job)
 
+def save_credentials(access_key, secret_key):
+	import keyring
+	keyring.set_password('AWS', access_key, secret_key)
+
 def set_connection_environment(access_key='0ZWJV1BMM1Q6GXJ9J2G2'):
 	"""
 	boto requires the credentials to be either passed to the connection,
@@ -55,46 +59,65 @@ def get_connection():
 		debug=True,
 		)
 
-def get_question():
-	from boto.mturk.question import Question, FreeTextAnswer, QuestionContent
+def get_questions():
+	from boto.mturk.question import (
+		Overview, FormattedContent, Question, FreeTextAnswer,
+		QuestionContent, List, QuestionForm,
+		)
+	form = QuestionForm()
+	o = Overview()
+	o.append_field('Title', 'Type a Page')
+	o.append_field('Text', 'You will read a scanned page and retype its textual contents. Here are some tips.')
+	instructions = List([
+		'You will need a PDF viewer. If you do not already have a PDF viewer, you can &lt;a href="http://get.adobe.com/reader/"&gt;download Adobe Reader&lt;/a&gt;',
+		'Please use your best judgement for including hand-written notes.',
+		'If you encounter something that is unrecognizable or unclear, do your best, then include three exclamation marks (!!!) to indicate that a problem occurred.',
+		'Please use exact capitalization spacing and punctuation.',
+		'In general, do not worry about formatting. Type each paragraph without carriage returns, and include a single carriage return between paragraphs.',
+		'If you encounter tables, type each row on the same line using the pipe (|) to separate columns.',
+		])
+	o.append(instructions)
+	url="http://drake.jaraco.com/docs/"
+	o.append(FormattedContent(
+		'The page is displayed below. If you prefer, you can use a '
+		'<a href="{url}">link to the page</a> to save the file or open '
+		'it in a separate window (using right-click and Save Link As or '
+		'Save Target As).'.format(**vars())))
+	#form.append(o)
+	
 	c = QuestionContent()
-	c.append('foo', 'How many movies have you watched this month?')
+	c.append_field("Text", "Type the content of the page here")
 	a = FreeTextAnswer()
-	q = Question('movies', c, a)
-	return q
+	q = Question('content', c, a)
+	form.append(q)
+	
+	c = QuestionContent()
+	c.append_field('Text', 'If you have any comments or questions, please include them here.')
+	a = FreeTextAnswer()
+	q = Question('comment', c, a)
+	#form.append(q)
+	return form
 
 def register_hit():
 	conn = get_connection()
 	from boto.mturk.price import Price
 	type_params = dict(
-		title="Movie Survey",
-		description="This is a survey to find out how many movies you have watched recently.",
-		#keywords='movies,subjective',
-		reward=Price(0.05),
+		title="Type a Page",
+		description="You will read a scanned page and retype its textual contents.",
+		#keywords='typing,page,rekey',
+		reward=Price(1.0),
+		questions = get_questions(),
 		)
 		
-	return conn.create_hit(question=get_question(), **type_params)
+	return conn.create_hit(**type_params)
 
-template = """<h1>Type a Page</h1>
-<p>Please re-type the content of the PDF page (a link is provided below). Please note,</p>
-<ul>
-    <li>You will need a PDF viewer. If you do not already have a PDF viewer, you can <a href="http://get.adobe.com/reader/">download Adobe Reader</a>.</li>
-    <li>Please use your best judgement for including hand-written notes.</li>
-    <li>If you encounter something that's unrecognizable or unclear, do your best, then include three exclamation marks (!!!) to indicate that a problem occurred.</li>
-    <li>Please use exact capitalization spacing and punctuation.</li>
-    <li>In general, do not worry about formatting. Type each paragraph without carriage returns, and include a single carriage return between paragraphs.</li>
-    <li>If you encounter tables, type each row on the same line using the pipe (|) to separate columns.</li>
-</ul>
-<p>The page is displayed below. If you prefer, you can use a <a href="${PAGE_URL}">link to the page</a> to save the file or open it in a separate window (using right-click and Save Link As or Save Target As).</p>
+template = """
 <p><iframe width="100%" height="50%" src="${PAGE_URL}">[Your browser does <em>not</em> support <code>iframe</code>,
 or has been configured not to display inline frames.
 You can access <a href="${PAGE_URL}">the document</a>
 via a link though.]</iframe></p>
-<p>Type the content of the page here.</p>
-<p><textarea rows="15" cols="80" name="content"></textarea></p>
-<p>If you have any comments or questions, please include them here.</p>
-<p><textarea rows="3" cols="80" name="comment"></textarea></p>
 """
 
 if __name__ == '__main__':
-	make_turk_recognition_job_from_pdf()
+	#make_turk_recognition_job_from_pdf()
+	register_hit()
