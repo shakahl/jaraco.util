571d05073164397c0c5ef0f11b69ac7ab4a23a30
jaraco <none@none>
2010-06-26 12:25:44 +0000

    Restored HTML template Enhanced command line handling (now pass 'serve' to run the server) Configured hit to register as an External Question so we can properly render the form.

--- jaraco/util/turk.py
+++ jaraco/util/turk.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import sys
+import optparse
 import subprocess
 import socket
 from glob import glob
@@ -59,22 +60,34 @@ def get_connection():
 		debug=True,
 		)
 
-def register_hit():
-	conn = get_connection()
-	from boto.mturk.price import Price
-	type_params = dict(
-		title="Type a Page",
-		description="You will read a scanned page and retype its textual contents.",
-		#keywords='typing,page,rekey',
-		reward=Price(1.0),
-		questions = RetypePageHIT.get_questions(),
-		)
-		
-	return conn.create_hit(**type_params)
-
 class RetypePageHIT:
+	def register(self):
+		conn = get_connection()
+		from boto.mturk.price import Price
+		type_params = dict(
+			title="Type a Page",
+			description="You will read a scanned page and retype its textual contents.",
+			keywords='typing page rekey'.split(),
+			reward=Price(1.0),
+			)
+			
+		return conn.create_hit(question=self.get_external_question(), **type_params)
+
+	@staticmethod
+	def get_external_question():
+		from boto.mturk.question import ExternalQuestion
+		external_url = 'http://drake.jaraco.com:8080/'
+		return ExternalQuestion(external_url=external_url, frame_height=600)
+		conn = get_connection()
+
 	@staticmethod
 	def get_questions():
+		"""
+		This techniuque attempts to use the amazon mturk api to construct
+		a QuestionForm suitable for performing the operation. Unfortunately,
+		it appears Amazon does not support inline PDF content.
+		http://developer.amazonwebservices.com/connect/thread.jspa?threadID=48210&tstart=0
+		"""
 		from boto.mturk.question import (
 			Overview, FormattedContent, Question, FreeTextAnswer,
 			QuestionContent, List, QuestionForm, AnswerSpecification,
@@ -115,17 +128,36 @@ class RetypePageHIT:
 		form.validate()
 		return form
 
-template = """		
-<p><iframe width="100%" height="50%" src="${PAGE_URL}">[Your browser does <em>not</em> support <code>iframe</code>,
+template = """
+<h1>Type a Page</h1>
+<p>Please re-type the content of the PDF page (a link is provided below). Please note,</p>
+<ul>
+    <li>You will need a PDF viewer. If you do not already have a PDF viewer, you can <a href="http://get.adobe.com/reader/">download Adobe Reader</a>.</li>
+    <li>Please use your best judgement for including hand-written notes.</li>
+    <li>If you encounter something that's unrecognizable or unclear, do your best, then include three exclamation marks (!!!) to indicate that a problem occurred.</li>
+    <li>Please use exact capitalization spacing and punctuation.</li>
+    <li>In general, do not worry about formatting. Type each paragraph without carriage returns, and include a single carriage return between paragraphs.</li>
+    <li>If you encounter tables, type each row on the same line using the pipe (|) to separate columns.</li>
+</ul>
+<p>The page is displayed below. If you prefer, you can use a <a href="{page_url}">link to the page</a> to save the file or open it in a separate window (using right-click and Save Link As or Save Target As).</p>
+<p><iframe width="100%" height="50%" src="{page_url}">[Your browser does <em>not</em> support <code>iframe</code>,
 or has been configured not to display inline frames.
-You can access <a href="${PAGE_URL}">the document</a>
+You can access <a href="{page_url}">the document</a>
 via a link though.]</iframe></p>
+<p>Type the content of the page here.</p>
+<form action="http://www.mturk.com/mturk/externalSubmit" method="POST">
+	<field type="hidden" value="{assignmentId}" name="assignmentId" />
+	<p><textarea rows="15" cols="80" name="content"></textarea></p>
+	<p>If you have any comments or questions, please include them here.</p>
+	<p><textarea rows="3" cols="80" name="comment"></textarea></p>
+</form>
 """
 
 
 class Server:
 	def index(self, hitId, assignmentId):
-		return '<div>Hit ID is %(hitId)s</div><div>Assignment ID is %(assignmentId)s</div>' % vars()
+		page_url = 'http://tbd'
+		return template.format(**vars())
 	index.exposed = True
 
 def start_server():
@@ -137,6 +169,15 @@ def start_server():
 	}
 	cherrypy.quickstart(Server(), config=config)
 
+def handle_command_line():
+	parser = optparse.OptionParser()
+	options, args = parser.parse_args()
+	if 'serve' in args:
+		start_server()
+		raise SystemExit(0)
+	hit = RetypePageHIT()
+	res = hit.register()
+	assert res.status == True
+
 if __name__ == '__main__':
-	#make_turk_recognition_job_from_pdf()
-	res = register_hit()
+	handle_command_line()
