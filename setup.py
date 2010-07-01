fecef2c3a04a6386f64791a686358c6919268acc
jaraco <none@none>
2010-07-01 22:37:46 +0000

    Bumped to 3.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ __date__ = '$Date$'[7:-2]
 name = 'jaraco.util'
 
 setup (name = name,
-		version = '3.3',
+		version = '3.4',
 		description = 'General utility modules that supply commonly-used functionality',
 		long_description = long_description,
 		author = 'Jason R. Coombs',
