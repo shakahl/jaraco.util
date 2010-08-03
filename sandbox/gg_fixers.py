7a96211cae8f4c598288067c4132d77bc9f25393
Jason R. Coombs <jaraco@jaraco.com>
2010-08-03 18:46:36 -0400

    More work on processing global giving data

--- sandbox/gg_fixers.py
+++ sandbox/gg_fixers.py
@@ -42,10 +42,78 @@ def fix_body(body):
 		body.text = fixed_text
 	body.text = body.text.replace('&amp;nbsp;', '')
 
+def get_sense_ids(senses):
+	"""
+	Take a list of SenseMakingItem tags and get the ids for each
+	
+	equivalent to
+	
+	ids = [sense.attrib['ID'] for sense in senses]
+	"""
+	ids = []
+	for sense in senses:
+		id = sense.attrib['ID']
+		ids.append(id)
+	return ids
+
+def get_location_for_sense(sense):
+	indexing = sense.find('Indexing')
+	questions = indexing.findall('QuestionIndex')
+	for question in questions:
+		# find the one with the ID S: STORY TAKES PLACE
+		if question.attrib['Id'] == 'S: STORY TAKES PLACE':
+			return question.find('Answer').text
+	# return None
+
+def assemble_rows(tree):
+	"""
+	Given a root element of a tree, return a list of rows suitable for
+	passing to write_data.
+	"""
+	senses = tree.getiterator('SenseMakingItem')
+	bodies = tree.getiterator('Body')
+	ids = get_sense_ids(senses)
+	locations = [get_location_for_sense(sense) for sense in senses]
+	body_texts = [body.text for body in bodies]
+	rows = zip(ids, locations, body_texts)
+	return rows
+
+def construct_rows(tree):
+	"""
+	Similar to assemble_rows, except is sense-oriented.
+	"""
+	senses = tree.getiterator('SenseMakignItem')
+	# create an empty list of row
+	rows = []
+	for sense in senses:
+		body_text = sense.find('Body').text
+		location = get_location_for_sense(sense)
+		id = sense.attrib['ID']
+		row = [id, location, body_text]
+		# append the new row to the list of accumulated rows
+		rows.append(row)
+	return rows
+
+def construct_row(sense):
+	"""
+	For a single sense, construct a row of the interesting columns"
+	"""
+	body_text = sense.find('Body').text
+	location = get_location_for_sense(sense)
+	id = sense.attrib['ID']
+	row = [id, location, body_text]
+	return row
+
+def construct_rows_functional(tree):
+	senses = tree.getiterator('SenseMakignItem')
+	# call construct row on each sense and return a list of the results
+	return map(construct_row, senses)
+
 def patch_xml_file(filename):
 	"""Read the xml file, fix the body tags, then save it to output.xml"""
+	global tree
 	tree = etree.parse(filename)
-	bodies = tree.iter('Body')
+	bodies = tree.getiterator('Body')
 	for body in bodies:
 		fix_body(body)
 	# re-write the modified xml back to a file
