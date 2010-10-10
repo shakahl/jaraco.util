633c3d68e4e79c01e18a671991156cbf42a27aaa
Jason R. Coombs <jaraco@jaraco.com>
2010-10-10 10:41:29 -0400

    Cleaned up setup.py, including removing make-turk-recognition-job

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-""" Setup script for building jaraco-util distribution
+"""
+Setup script for building jaraco.util distribution
 
 Copyright © 2004-2010 Jason R. Coombs
 """
@@ -16,53 +17,52 @@ try:
 except ImportError:
 	from distutils.command.build_py import build_py
 
-from setuptools import setup, find_packages
-
-__author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
-__version__ = '$Rev$'[6:-2]
-__svnauthor__ = '$Author$'[9:-2]
-__date__ = '$Date$'[7:-2]
+from setuptools import find_packages
 
 name = 'jaraco.util'
 
-setup (name = name,
-		use_hg_version_increment='0.1',
-		description = 'General utility modules that supply commonly-used functionality',
-		long_description = open('README').read(),
-		author = 'Jason R. Coombs',
-		author_email = 'jaraco@jaraco.com',
-		url = 'http://pypi.python.org/pypi/'+name,
-		packages = find_packages(exclude=['tests']),
-		namespace_packages = ['jaraco',],
-		license = 'MIT',
-		classifiers = [
-			"Development Status :: 4 - Beta",
-			"Intended Audience :: Developers",
-			"Programming Language :: Python",
-			"Programming Language :: Python :: 3",
-		],
-		entry_points = {
-			'console_scripts': [
-				'roll-dice = jaraco.util.dice:do_dice_roll',
-				'release-package = jaraco.util.package:release',
-				'calc-prorate = jaraco.dateutil:calculate_prorated_values',
-				'make-turk-recognition-job = jaraco.util.turk:make_turk_recognition_job_from_pdf',
-				],
-		},
-		install_requires=[
-		],
-		extras_require = {
-			'image':
-				['pil>=1.1.6'],
-		},
-		dependency_links = [
-		],
-		tests_require=[
-			'nose>=0.10',
-		],
-		test_suite = "nose.collector",
-		setup_requires=[
-			'hgtools>=0.4',
-		],
-		cmdclass=dict(build_py=build_py),
-	)
+setup_params = dict(
+	name = name,
+	use_hg_version_increment='0.1',
+	description = 'General utility modules that supply commonly-used functionality',
+	long_description = open('README').read(),
+	author = 'Jason R. Coombs',
+	author_email = 'jaraco@jaraco.com',
+	url = 'http://pypi.python.org/pypi/'+name,
+	packages = find_packages(exclude=['tests']),
+	namespace_packages = ['jaraco',],
+	license = 'MIT',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points = {
+		'console_scripts': [
+			'roll-dice = jaraco.util.dice:do_dice_roll',
+			'release-package = jaraco.util.package:release',
+			'calc-prorate = jaraco.dateutil:calculate_prorated_values',
+			],
+	},
+	install_requires=[
+	],
+	extras_require = {
+		'image':
+			['pil>=1.1.6'],
+	},
+	dependency_links = [
+	],
+	tests_require=[
+		'nose>=0.10',
+	],
+	test_suite = "nose.collector",
+	setup_requires=[
+		'hgtools>=0.4',
+	],
+	cmdclass=dict(build_py=build_py),
+)
+
+if __name__ == '__main__':
+	from setuptools import setup
+	setup(**setup_params)
