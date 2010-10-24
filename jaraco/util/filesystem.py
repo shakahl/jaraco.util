5f133a7b2ddd62d0d2037f0bd2904dea8c4d2c75
Jason R. Coombs <jaraco@jaraco.com>
2010-10-23 20:24:56 -0400

    Added ensure_dir_exists decorator

--- jaraco/util/filesystem.py
+++ jaraco/util/filesystem.py
@@ -84,3 +84,13 @@ def ExtensionReplacer(new_ext):
 	'myfile.pdf'
 	"""
 	return functools.partial(replace_extension, new_ext)
+
+def ensure_dir_exists(func):
+	"wrap a function that returns a dir, making sure it exists"
+	@functools.wraps(func)
+	def make_if_not_present():
+		dir = func()
+		if not os.path.isdir(dir):
+			os.makedirs(dir)
+		return dir
+	return make_if_not_present
