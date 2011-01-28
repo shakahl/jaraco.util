1bb28441a25843ccc662fd0b5a6ade4b99c4c9e8
Jason R. Coombs <jaraco@jaraco.com>
2011-01-28 14:30:07 -0500

    Updated bug to include fix in Python 3.2

--- bugs/string-formatter.py
+++ bugs/string-formatter.py
@@ -4,9 +4,12 @@ class MyDynamicObject:
 		return name + ' ' + str(random.randint(1,10))
 
 print("%(foo)s" % MyDynamicObject()) # works!
-print("{foo}".format(**MyDynamicObject()))
-# can't do that because MyDynamicObject can't enumerate every possible
-#  kwparam
-# fails with TypeError: format() argument after ** must be a mapping,
-#  not instance.
+if hasattr('', 'format_map'):
+	print("{foo}".format_map(MyDynamicObject())) # works on Python 3.2+
+else:
+	print("{foo}".format(**MyDynamicObject()))
+	# can't do that because MyDynamicObject can't enumerate every possible
+	#  kwparam
+	# fails with TypeError: format() argument after ** must be a mapping,
+	#  not instance.
 
