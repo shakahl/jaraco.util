b573dbc78b95f5dbb9c52b932c7dd094edd73a67
Jason R. Coombs <jaraco@jaraco.com>
2011-03-21 19:16:10 -0400

    Adding cross-platform getch function

---
+++ jaraco/util/input.py
@@ -0,0 +1,15 @@
+
+try:
+	# Win32
+	from msvcrt import getch
+except ImportError:
+	# UNIX
+	def getch():
+		import sys, tty, termios
+		fd = sys.stdin.fileno()
+		old = termios.tcgetattr(fd)
+		try:
+			tty.setraw(fd)
+			return sys.stdin.read(1)
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, old)
