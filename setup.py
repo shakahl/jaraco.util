a9c23aef6f3a0938b20d964bbf6064a1c7866938
Jason R. Coombs <jaraco@jaraco.com>
2011-08-21 14:02:41 -0600

    Updated trove classifiers to exclude earlier versions of Python 2

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ setup_params = dict(
 	classifiers = [
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: Developers",
-		"Programming Language :: Python",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3",
 	],
 	entry_points = {
