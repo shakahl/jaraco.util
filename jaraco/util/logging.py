01a148f50856430e48cbf704818ce41031b120b0
Jason R. Coombs <jaraco@jaraco.com>
2011-08-22 22:59:08 -0600

    Cleaned up logging module

--- jaraco/util/logging.py
+++ jaraco/util/logging.py
@@ -6,10 +6,6 @@ import warnings
 
 from jaraco import dateutil
 
-def add_options(parser):
-	warnings.warn("add_options is deprecated. use add_arguments", DeprecationWarning)
-	return add_arguments(parser)
-
 def add_arguments(parser):
 	"""
 	Add arguments to an ArgumentParser or OptionParser for purposes of
@@ -35,64 +31,65 @@ class TimestampFileHandler(logging.StreamHandler):
 	logging.handlers.RotatingFileHandler, but instead of
 	appending a number, uses a timestamp to periodically select
 	new file names to log to.
-	
+
 	Since this was developed, a TimedRotatingFileHandler was added to
-	the Python stdlib. This class is still useful because it 
+	the Python stdlib. This class is still useful because it allows the period
+	to be specified using simple english words.
 	"""
-	def __init__(self, baseFilename, mode='a', period='day'):
-		self.baseFilename = baseFilename
+	def __init__(self, base_filename, mode='a', period='day'):
+		self.base_filename = base_filename
 		self.mode = mode
-		self._setPeriod(period)
+		self._set_period(period)
 		logging.StreamHandler.__init__(self, None)
 
-	def _setPeriod(self, period):
+	def _set_period(self, period):
 		"""
-		Set the period for the timestamp.  If period is 0 or None, no period will be used.
+		Set the period for the timestamp.  If period is 0 or None, no period
+		will be used.
 		"""
 		self._period = period
 		if period:
-			self._periodSeconds = dateutil.get_period_seconds(self._period)
-			self._dateFormat = dateutil.get_date_format_string(self._periodSeconds)
+			self._period_seconds = dateutil.get_period_seconds(self._period)
+			self._date_format = dateutil.get_date_format_string(
+				self._period_seconds)
 		else:
-			self._periodSeconds = 0
-			self._dateFormat = ''
+			self._period_seconds = 0
+			self._date_format = ''
 
-	def _getPeriod(self):
+	def _get_period(self):
 		return self._period
-	period = property(_getPeriod, _setPeriod)
-	
-	def _useFile(self, filename):
-		self._ensureDirectoryExists(filename)
+	period = property(_get_period, _set_period)
+
+	def _use_file(self, filename):
+		self._ensure_directory_exists(filename)
 		self.stream = open(filename, self.mode)
 
-	def _ensureDirectoryExists(self, filename):
+	def _ensure_directory_exists(self, filename):
 		dirname = os.path.dirname(filename)
 		if dirname and not os.path.exists(dirname):
 			os.makedirs(dirname)
 
-	def getFilename(self, t):
+	def get_filename(self, t):
 		"""
 		Return the appropriate filename for the given time
 		based on the defined period.
 		"""
-		root, ext = os.path.splitext(self.baseFilename)
+		root, ext = os.path.splitext(self.base_filename)
 		# remove seconds not significant to the period
-		if self._periodSeconds:
-			t -= t % self._periodSeconds
+		if self._period_seconds:
+			t -= t % self._period_seconds
 		# convert it to a datetime object for formatting
 		dt = datetime.datetime.utcfromtimestamp(t)
 		# append the datestring to the filename
 		# workaround for datetime.strftime not handling '' properly
-		if not self._dateFormat == '':
-			appendedDate = dt.strftime(self._dateFormat)
-		else:
-			appendedDate = ''
-		if appendedDate:
+		appended_date = (dt.strftime(self._date_format)
+			if self._date_format != '' else '')
+		if appended_date:
 			# in the future, it would be nice for this format
 			#  to be supplied as a parameter.
-			result = root + ' ' + appendedDate + ext
+			result = root + ' ' + appended_date + ext
 		else:
-			result = self.baseFilename
+			result = self.base_filename
 		return result
 
 	def emit(self, record):
@@ -103,13 +100,13 @@ class TimestampFileHandler(logging.StreamHandler):
 		opened file has the correct date.
 		"""
 		now = time.time()
-		currentName = self.getFilename(now)
+		current_name = self.get_filename(now)
 		try:
-			if not self.stream.name == currentName:
-				self._useFile(currentName)
+			if not self.stream.name == current_name:
+				self._use_file(current_name)
 		except AttributeError:
 			# a stream has not been created, so create one.
-			self._useFile(currentName)
+			self._use_file(current_name)
 		logging.StreamHandler.emit(self, record)
 
 	def close(self):
@@ -125,7 +122,7 @@ class LogFileWrapper(object):
 	Emulates a file to replace stdout or stderr or
 	anothe file object and redirects its output to
 	a logger.
-	
+
 	Since data will often be send in partial lines or
 	multiple lines, data is queued up until a new line
 	is received.  Each line of text is send to the
