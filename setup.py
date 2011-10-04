21bdac6803f94c80f1ec1b8b10a90293d90337dc
Jason R. Coombs <jaraco@jaraco.com>
2011-10-04 11:35:58 -0400

    Now use use_2to3_exclude_fixers to exclude import fixer

--- setup.py
+++ setup.py
@@ -3,35 +3,13 @@
 """
 Setup script for building jaraco.util distribution
 
-Copyright © 2004-2010 Jason R. Coombs
+Copyright © 2004-2011 Jason R. Coombs
 """
 
 import sys
 import subprocess
 
 from setuptools import find_packages, Command
-
-import setuptools.command.build_py
-
-def install_fixer_names():
-	"""
-	Distribute doesn't provide an exemption mechanism, so we need to build
-	the full list as if distribute had done it, then we can skip the fixers
-	we want to exempt.
-	"""
-	# if we're not on Python 3, do nothing - this routine can cause
-	#  problems, so don't run it if we're not going to need it.
-	if sys.version_info < (3,): return
-	if not hasattr(setuptools, 'lib2to3_fixer_packages'): return
-	names = getattr(setuptools.command.build_py.build_py, 'fixer_names', None) or []
-	from lib2to3.refactor import get_fixers_from_package
-	for p in setuptools.lib2to3_fixer_packages:
-		names.extend(get_fixers_from_package(p))
-	setuptools.command.build_py.build_py.fixer_names = names
-
-def skip_fixer(fixer_name):
-	names = getattr(setuptools.command.build_py.build_py, 'fixer_names', None)
-	names and names.remove(fixer_name)
 
 class PyTest(Command):
 	user_options = []
@@ -87,10 +65,13 @@ setup_params = dict(
 		test=PyTest,
 	),
 	use_2to3=True,
+	use_2to3_exclude_fixers=['lib2to3.fixes.fix_import'],
 )
 
 if __name__ == '__main__':
 	from setuptools import setup
-	install_fixer_names()
-	skip_fixer('lib2to3.fixes.fix_import')
+	import pkg_resources
+	if sys.version_info >= (3,):
+		# distribute 0.6.24 is required to exclude the fixers
+		pkg_resources.require('distribute>=0.6.24dev')
 	setup(**setup_params)
