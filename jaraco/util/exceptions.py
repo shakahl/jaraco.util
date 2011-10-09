d4657dcff3c8320e37606d19c5b42adeba5ce1e5
Jason R. Coombs <jaraco@jaraco.com>
2011-10-09 03:52:32 -0400

    Added jaraco.util.exceptions.suppress_exception

--- jaraco/util/exceptions.py
+++ jaraco/util/exceptions.py
@@ -1,7 +1,7 @@
 def throws_exception(callable, exception=Exception):
 	"""
 	Return True if the callable throws the specified exception
-	
+
 	>>> throws_exception(lambda: int('3'))
 	False
 	>>> throws_exception(lambda: int('a'))
@@ -16,3 +16,22 @@ def throws_exception(callable, exception=Exception):
 	except Exception:
 		pass
 	return False
+
+def suppress_exception(callable, *exceptions):
+	"""
+	Call `callable` and return its result but suppress any specified
+	exceptions (returning None).
+
+	>>> suppress_exception(lambda: int('3'))
+	3
+	>>> suppress_exception(lambda: int('a'))
+	>>> suppress_exception(lambda: int('a'), KeyError)
+	Traceback (most recent call last):
+	...
+	ValueError: invalid literal for int() with base 10: 'a'
+	"""
+	if not exceptions: exceptions = (Exception,)
+	try:
+		return callable()
+	except exceptions:
+		pass
