63440a235066c09e177719993df44bdeede6eb86
Jason R. Coombs <jaraco@jaraco.com>
2012-01-25 19:48:46 -0500

    Enabled custom parameters in logging.setup

--- jaraco/util/logging.py
+++ jaraco/util/logging.py
@@ -6,6 +6,12 @@ import warnings
 
 from jaraco import dateutil
 
+def log_level(level_string):
+	"""
+	Return a log level for a string
+	"""
+	return getattr(logging, level_string.upper())
+
 def add_arguments(parser):
 	"""
 	Add arguments to an ArgumentParser or OptionParser for purposes of
@@ -15,15 +21,18 @@ def add_arguments(parser):
 		getattr(parser, 'add_argument', None)
 		or getattr(parser, 'add_option')
 	)
-	adder('-l', '--log-level', default='info',
+	adder('-l', '--log-level', default=logging.INFO, type=log_level,
 		help="Set log level (DEBUG, INFO, WARNING, ERROR)")
 
-def setup(options):
+def setup(options, **kwargs):
 	"""
 	Setup logging with options or arguments from an OptionParser or
-	ArgumentParser
+	ArgumentParser. Also pass any keyword arguments to the basicConfig
+	call.
 	"""
-	logging.basicConfig(level=getattr(logging, options.log_level.upper()))
+	params = dict(kwargs)
+	params.update(level=options.log_level)
+	logging.basicConfig(**params)
 
 class TimestampFileHandler(logging.StreamHandler):
 	"""
