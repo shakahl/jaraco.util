b9d32e2f1b740ab93cf02a8a8d9e10c61a7fda18
Jason R. Coombs <jaraco@jaraco.com>
2012-01-27 21:21:45 -0500

    Fixed encoding error in auth.PasswordGenerator

--- jaraco/util/auth.py
+++ jaraco/util/auth.py
@@ -10,7 +10,14 @@ class PasswordGenerator(object):
 	>>> pw = PasswordGenerator.make_password(8, encoding=None)
 	>>> len(pw)
 	8
+
 	>>> pw != PasswordGenerator.make_password(8, encoding=None)
+	True
+
+	>>> pw = PasswordGenerator.make_password(8, encoding='hex')
+	>>> type(pw) == str
+	True
+	>>> set(pw) <= set('0123456789abcdef')
 	True
 	"""
 
@@ -18,7 +25,7 @@ class PasswordGenerator(object):
 	def make_password(n_bytes = 8, encoding = 'base-64'):
 		'Make a password with n_bytes of disorder; optionally encoded'
 		chars = PasswordGenerator.get_random_chars(n_bytes)
-		result = ''.join(chars)
+		result = ''.join(chars).encode('latin-1')
 		null_encoder = lambda s: (s, len(s))
 		encoder = codecs.getencoder(encoding) if encoding else null_encoder
 		encoded, length = encoder(result)
