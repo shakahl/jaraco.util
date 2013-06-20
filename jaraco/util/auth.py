e123d259ab08069c1e3b2850feee94eb74a806d0
Jason R. Coombs <jaraco@jaraco.com>
2013-06-20 08:27:02 -0500

    Fix regression on Python 2

--- jaraco/util/auth.py
+++ jaraco/util/auth.py
@@ -5,11 +5,11 @@ import random
 import itertools
 import binascii
 
+from . import six
+
 class PasswordGenerator(object):
 	"""
 	Generates random passwords
-
-	>>> from jaraco.util import six
 
 	>>> pw = PasswordGenerator.make_password(8, encoding=None)
 	>>> len(pw)
@@ -29,7 +29,7 @@ class PasswordGenerator(object):
 	def make_password(n_bytes = 8, encoding = 'base-64'):
 		'Make a password with n_bytes of disorder; optionally encoded'
 		chars = PasswordGenerator.get_random_chars(n_bytes)
-		result = ''.join(chars).encode('latin-1')
+		result = b''.join(chars)
 		if encoding == 'hex':
 			return binascii.hexlify(result)
 		null_encoder = lambda s: (s, len(s))
@@ -44,4 +44,4 @@ class PasswordGenerator(object):
 	@staticmethod
 	def random_byte_generator():
 		while True:
-			yield chr(random.randint(0, 255))
+			yield six.int2byte(random.randint(0, 255))
